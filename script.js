91ce28db733d74613c7804aa36e2bf7f50073e38
ajrooter <kumar11.v@gmail.com>
2021-12-26 19:40:05 +0530

    updated script,index files

---
+++ script.js
@@ -0,0 +1,49 @@
+function computerPlay()
+{
+    let lis = ["rock","paper","scissors"];
+    let randcolor =  lis[Math.floor(Math.random() * lis.length)];
+    return randcolor;
+}
+computerSelection = computerPlay();
+let playerSelection = 'rock';//window.prompt("Enter: rock or paper or scissors");//
+playerSelection.toLocaleLowerCase();
+
+
+
+function playRound(c ,p)
+{
+    if(c == "rock" && p == "paper")
+        {
+            console.log('You win, '+ p+ ' beats '+ c );
+        }
+    else if( c =="rock" && p=="sciccors")
+        {
+            console.log('You lose, '+ c + ' beats '+p );
+        }
+    else if( c == p)
+        {
+            console.log("It's a draw");
+        }
+    else if( c =="paper" && p=="sciccors")
+        {
+            console.log('You win, '+ p + ' beats '+c );
+        }   
+    else if( c =="paper" && p=="rock")
+        {
+            console.log('You lose, '+ c + ' beats '+p );
+        }  
+    else if( c =="scissors" && p=="paper")
+        {
+            console.log('You lose, '+ c + ' beats '+p );
+        }   
+    else if( c =="scissors" && p=="rock")
+        {
+            console.log('You win, '+ p + ' beats '+c );
+        }   
+        
+     
+} 
+
+let m = computerSelection;
+let n = playerSelection;
+playRound(m,n)   ;
