ccd0d04503d1d5b97a8cb62dcf10a548ef8ce8cd
ajrooter <kumar11.v@gmail.com>
2022-01-11 10:46:43 +0530

    First branch

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ function computerPlay()
     return randcolor;
 }
 computerSelection = computerPlay();
-let playerSelection = window.prompt("Enter: rock or paper or scissors");//User is aksed to give rock or paper or scissors
+//let playerSelection = window.prompt("Enter: rock or paper or scissors");//User is aksed to give rock or paper or scissors
 //limitation could be what user enters, a selexted list of permitted codewords can be included
-playerSelection.toLocaleLowerCase();//
+//playerSelection.toLocaleLowerCase();//
 
 let count; // to keep track of the number of user wins
 
 function playRound(c ,p)  // Main function that compares user's choice and computer. ie THE GAME
 {
+
+        c = computerPlay();
     if(c == "rock" && p == "paper")
         {
             console.log('You win, '+ p+ ' beats '+ c );
             count+=1;
         }
-    else if( c =="rock" && p=="sciccors")
+    else if( c =="rock" && p=="scissors")
         {
             console.log('You lose, '+ c + ' beats '+p );
         }
@@ -26,7 +28,7 @@ function playRound(c ,p)  // Main function that compares user's choice and compu
         {
             console.log("It's a draw");
         }
-    else if( c =="paper" && p=="sciccors")
+    else if( c =="paper" && p=="scissors")
         {
             console.log('You win, '+ p + ' beats '+c );
             count+=1;
@@ -48,14 +50,47 @@ function playRound(c ,p)  // Main function that compares user's choice and compu
         console.log("You have entered an invalid option")
     }  
         
-     
+    //console.log("The consolidated no of wins:"+ `${count}`);//not sure of correct, let's see
 } 
 
 let m = computerSelection;
-let n = playerSelection;
+//let n = playerSelection;
 
 
-for (let i = 0; i < 5; i++) { //loops  times to indicate that there are  rounds, coudl give user more granularity on the number of games to be played
-    playRound(m,n);
-  } 
-console.log("The consolidated no of wins:"+ ${count});//not sure of correct, let's see
+//for (let i = 0; i < 5; i++) { //loops  times to indicate that there are  rounds, coudl give user more granularity on the number of games to be played
+   // playRound(m,n);
+ // } 
+
+
+function rockpaperscissor(n)
+{
+    
+        playRound(m,n);
+        
+       
+}
+
+const div1 = document.createElement('div')
+const score = document.createTextNode('Your score is: '+ `${count}`)
+div1.appendChild(score)
+const men = document.querySelector('.f5')
+men.appendChild(div1)
+
+
+
+//The embedding it to the html aspect of web design.
+
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', () =>rockpaperscissor('rock'));
+
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', () => rockpaperscissor('paper'));
+
+const scissors = document.querySelector('.scissor');
+scissors.addEventListener('click', () => rockpaperscissor('scissors'));
+
+
+
+
+
+
